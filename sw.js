f1201048964d58930b0ecb1a60fa0fe8a2a249e2
Gustavo Prestes <gustavopres@gmail.com>
2018-08-20 22:21:21 -0300

    asdasdasdasda

--- sw.js
+++ sw.js
@@ -1,20 +1,22 @@
 var CACHE_NAME = 'static-v1';
 self.addEventListener('install', function (event) {
- event.waitUntil(
-   caches.open(CACHE_NAME).then(function (cache) {
-     return cache.addAll([
-       '/',
-       '/index.html',
-       '/todo.css',
-       '/app.js',
-       '/manifest.json',
-     ]);
-   })
-   self.addEventListener('activate', function activator(event) {
-     event.waitUntil(caches.keys().then(function (keys) {
-       return Promise.all(keys.filter(function (key) {
-         return key.indexOf(CACHE_NAME) !== 0;}).map(function (key) {
-           return caches.delete(key);
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/todo.css',
+        '/app.js',
+        '/manifest.json',
+      ]);
+    })
+  );
+});
+  self.addEventListener('activate', function activator(event) {
+    event.waitUntil(caches.keys().then(function (keys) {
+      return Promise.all(keys.filter(function (key) {
+        return key.indexOf(CACHE_NAME) !== 0;}).map(function (key) {
+            return caches.delete(key);
          })
        );
      })
